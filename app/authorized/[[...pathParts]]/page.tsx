3c0cfc48f372294d4d82a36137bcc39631e1eb5e
adambujdak1225 <514381@mail.muni.cz>
2024-03-23 13:02:02 +0100

    remove middleware

--- app/authorized/[[...pathParts]]/page.tsx
+++ app/authorized/[[...pathParts]]/page.tsx
@@ -1,6 +1,14 @@
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
 import AuthorizedLayout from "./AuthorizedLayout";
 
 export default async function AuthorizedPage() {
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user) {
+    redirect("/");
+  }
+
   return (
     <AuthorizedLayout />
   );
