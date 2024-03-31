2c5700367f85e5c9839e415642596cf88bb2978a
adambujdak1225 <514381@mail.muni.cz>
2024-03-31 22:12:36 +0200

    case tree fix event propagation

--- app/components/CaseTree.tsx
+++ app/components/CaseTree.tsx
@@ -18,8 +18,8 @@ const CaseTree = ({root, rootLink, hierarchy }: Props) => {
           hierarchy.items as CaseHierarchy[]).map((item) => (
             <li key={item.levelName} className="max-w-full truncate">
               <details open={item.lastLevel !== undefined && !item.lastLevel} className="max-w-full *:open:after:rotate-[45deg] *:open:after:mt-[-0.5rem] *:after:rotate-[-45deg] *:after:mt-[-0.25rem]">
-                <summary className="flex flex-row-reverse justify-end py-0 active:!bg-gray-50 hover:bg-gray-50">
-                  <Redirect link={`${rootLink}/${item.levelName}`} className="flex-1" shallow>
+                <summary className="flex flex-row-reverse justify-end py-0 active:!bg-white hover:bg-white">
+                  <Redirect link={`${rootLink}/${item.levelName}`} className="flex-1" shallow parentElementDetails>
                     <div className="truncate hover:bg-gray-200 rounded-md px-[0.375rem] py-[0.25rem]">{item.levelName}</div>
                   </Redirect>
                   {/* <Image
--- app/components/Redirect/Redirect.tsx
+++ app/components/Redirect/Redirect.tsx
@@ -6,19 +6,24 @@ type Props = {
   link: string,
   shallow: boolean,
   propagateEvent?: boolean, 
+  parentElementDetails?: boolean,
   className?: string,
 }
 
-export const shallowRedirect = (e: React.MouseEvent,link: string, propagate: boolean) => {
-  if (!propagate) {
-    e.preventDefault()
+export const shallowRedirect = (e: React.MouseEvent,link: string, propagate: boolean, parentElementDetails: boolean) => {
+  if (parentElementDetails) {
+    if((e.currentTarget.parentElement?.parentElement as HTMLDetailsElement).open) {
+      e.preventDefault();
+    }
+  } else if (!propagate) {
+    e.preventDefault();
   }
   window.history.pushState(null, '', `${link}`)
 }
 
-const Redirect = ({ children, link, shallow, propagateEvent=false, className }: Props) => {
+const Redirect = ({ children, link, shallow, propagateEvent=false, parentElementDetails=false, className }: Props) => {
   if (shallow) {
-    return (<div onClick={(e) => shallowRedirect(e, link, propagateEvent)} className={className}>{children}</div>);
+    return (<div onClick={(e) => shallowRedirect(e, link, propagateEvent, parentElementDetails)} className={className}>{children}</div>);
   }
   return (
     <Link prefetch={false} href={link} className={className}>
