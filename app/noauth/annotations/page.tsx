7ea93a2e2f88e6189f59f023d57c16c6a2c5474f
adambujdak1225 <514381@mail.muni.cz>
2024-04-03 19:09:31 +0200

    fix

--- app/noauth/annotations/page.tsx
+++ app/noauth/annotations/page.tsx
@@ -1,5 +1,3 @@
-import AnnotationButtons from '@/app/authorized/annotations/components/Annotations/AnnotationButtons'
-import AnnotationPresetGrid from '@/app/authorized/annotations/components/Annotations/AnnotationPresetGrid'
 import React from 'react'
 
 const AnnotationsPage = () => {
@@ -8,9 +6,7 @@ const AnnotationsPage = () => {
       <div className='flex flex-col items-center gap-2 md:w-5/6 lg:w-9/12'>
         <div className='w-full flex justify-between items-center gap-2'>
           <div className='font-sans font-semibold text-slate-500 text-2xl pl-1'>Annotations presets</div>
-          <AnnotationButtons />
         </div>
-        <AnnotationPresetGrid />
       </div>
     </div>
   )
