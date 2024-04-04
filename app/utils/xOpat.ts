5b46937f1a9fb99168ea7b9f1b12eb7e592e12c9
adambujdak1225 <514381@mail.muni.cz>
2024-04-04 14:14:46 +0200

    open xOpat always in new window

--- app/utils/xOpat.ts
+++ app/utils/xOpat.ts
@@ -2,7 +2,7 @@ export const openXOpat = async (visualization: object) => {
   const xOpatUrl = process.env.NEXT_PUBLIC_XOPAT_URL || "";
   const method = process.env.NEXT_PUBLIC_XOPAT_OPEN_METHOD || "GET"
   if(method === 'GET') {
-    window.open(encodeURI(`${xOpatUrl}/index.php#${JSON.stringify(visualization)}`), "_viewer")
+    window.open(encodeURI(`${xOpatUrl}/index.php#${JSON.stringify(visualization)}`), "_blank", )
   }
   // TODO implement POST alternative
 }
