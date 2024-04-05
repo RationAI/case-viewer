ee7ac3121008df7e414690974f3e593834110de0
adambujdak1225 <514381@mail.muni.cz>
2024-04-05 11:55:18 +0200

    try visited state

--- app/authorized/cases/components/Table/TableRows/FileRow.tsx
+++ app/authorized/cases/components/Table/TableRows/FileRow.tsx
@@ -35,54 +35,68 @@ const handleOpenInXOpat = (event: React.FormEvent<HTMLFormElement>, slide: Table
   console.log(JSON.stringify("No visualization, cannot open xOpat"))
 }
 
+const markSlideVisited = (event, href: string) => {
+  console.log(href)
+  event.preventDefault();
+  if (history.replaceState) {
+    const current_url = window.location.href;
+    history.replaceState({},'',href);
+    history.replaceState({},'',current_url);
+  }
+  // TODO fix
+}
+
 const FileRow = ({ slide, rowNo }: Props) => {
+  const slideHref = `/cache-${process.env.NEXT_PUBLIC_CACHE_KEY}/${slide.slideId}`
 
   return (
-    <div key={slide.slideId} className="collapse collapse-arrow border rounded-sm overflow-visible">
-      <input type="checkbox" />
-      <div className="collapse-title flex flex-row gap-4 py-0 pl-0">
-        <ImagePreview modalId={"modalId" + slide.slideId} slideId={slide.slideId} />
-        <form className="flex-1 flex flex-row gap-4" onSubmit={(e) => handleOpenInXOpat(e, slide)}>
-          <div className="flex flex-col justify-center min-w-[18rem] flex-1 px-2">
-            <p className="font-bold">{slide.name}</p>
-            <p className="">{slide.created}</p>
+    <a href={slideHref} className="block border rounded-sm visited:border-fuchsia-500" onClick={(e) => markSlideVisited(e, slideHref)}>
+      <div key={slide.slideId} className="collapse collapse-close overflow-visible"> {/* collapse-arrow */}
+        <input type="checkbox" />
+        <div className="collapse-title flex flex-row gap-4 py-0 pl-0">
+          <ImagePreview modalId={"modalId" + slide.slideId} slideId={slide.slideId} />
+          <form className="flex-1 flex flex-row gap-4" onSubmit={(e) => handleOpenInXOpat(e, slide)}>
+            <div className="flex flex-col justify-center min-w-[18rem] flex-1 px-2">
+              <p className="font-bold">{slide.name}</p>
+              <p className="">{slide.created}</p>
+            </div>
+            {slide.masks &&
+              <div className="flex w-[12.5rem] items-center justify-center z-10">
+                <FileRowSelect options={slide.masks.map((mask) => mask.name)}/>
+              </div>
+            }
+            {slide.annotations &&
+              <div className="flex items-center justify-center z-10 px-[0.375rem]">
+                <ImageGrid
+                  images={slide.annotations}
+                  count={9}
+                  typeName="annotation"
+                  rowNo={rowNo}
+                />
+              </div>
+            }
+            {slide.visualizationConfig &&
+              <div className="flex w-[12.5rem] items-center justify-center z-10">
+                <FileRowSelect options={slide.visualizationConfig.visualizations.map((vis) => vis.name)} />
+              </div>
+            }
+            <div className="w-16 z-10">
+              <FileRowActions slidePath={`${slide.casePath}/${slide.slideId}`} />
+            </div>
+          </form>
+        </div>
+        <div className="collapse-content px-2">
+          <div className="pt-2">
+            {Object.entries(slide.metadata).map(([key, value]) => (
+              <div key={key} className="flex gap-1">
+                <p className="font-medium">{key + ":"}</p>
+                <p>{value}</p>
+              </div>
+            ))}
           </div>
-          {slide.masks &&
-            <div className="flex w-[12.5rem] items-center justify-center z-10">
-              <FileRowSelect options={slide.masks.map((mask) => mask.name)}/>
-            </div>
-          }
-          {slide.annotations &&
-            <div className="flex items-center justify-center z-10 px-[0.375rem]">
-              <ImageGrid
-                images={slide.annotations}
-                count={9}
-                typeName="annotation"
-                rowNo={rowNo}
-              />
-            </div>
-          }
-          {slide.visualizationConfig &&
-            <div className="flex w-[12.5rem] items-center justify-center z-10">
-              <FileRowSelect options={slide.visualizationConfig.visualizations.map((vis) => vis.name)} />
-            </div>
-          }
-          <div className="w-16 z-10">
-            <FileRowActions slidePath={`${slide.casePath}/${slide.slideId}`} />
-          </div>
-        </form>
-      </div>
-      <div className="collapse-content px-2">
-        <div className="pt-2">
-          {Object.entries(slide.metadata).map(([key, value]) => (
-            <div key={key} className="flex gap-1">
-              <p className="font-medium">{key + ":"}</p>
-              <p>{value}</p>
-            </div>
-          ))}
         </div>
       </div>
-    </div>
+    </a>
   );
 };
 
